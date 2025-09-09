64f2b73505968952cdb9c00185d9d03d4c25808d
SYJ1029 <abg0986@gmail.com>
2025-09-09 10:33:16 +0900

    넷겜플 1장

--- MyThread/MyThread/Helloworld.cpp
+++ MyThread/MyThread/Helloworld.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <thread>
 
 
+int sum;
+void worker()
+{
+	for (auto i = 0; i < 25000000; ++i)
+		sum = sum + 2;
+}
 
-int main() {
 
-	std::cout << "Hello World!" << std::endl;
+int main()
+{
+	std::thread t1(worker);
+	std::thread t2(worker);
+	t1.join();
+	t2.join();
+	std::cout << "Sum = " << sum << std::endl;
 }
