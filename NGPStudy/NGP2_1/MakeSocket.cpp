4d625f67697e71fd58c6e805ed9020dacb347fdb
SYJ1029 <abg0986@gmail.com>
2025-09-09 18:37:06 +0900

    MAKEWORD 없이

---
+++ NGPStudy/NGP2_1/MakeSocket.cpp
@@ -0,0 +1,46 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <winsock2.h>
+#include <stdio.h>
+
+
+#pragma comment(lib, "ws2_32") // ws2_32.lib 링크
+
+
+void err_quit(const char* msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	MessageBoxA(NULL, (const char*)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+
+int main(int argc, char* argv[])
+{
+	// 윈속 초기화
+	WSADATA wsa;
+	WORD word = MAKEWORD(1, 1);
+
+	if (WSAStartup(word, &wsa) != 0)
+		return 1;
+	printf("[알림] 윈속 초기화 성공\n");
+	printf("%#x, %#x, %s, %s\n", wsa.wVersion, wsa.wHighVersion, wsa.szDescription, wsa.szSystemStatus);
+
+	// 소켓 생성
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET) err_quit("socket()");
+	printf("[알림] 소켓 생성 성공\n");
+
+	// 소켓 닫기
+	closesocket(sock);
+
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+}
