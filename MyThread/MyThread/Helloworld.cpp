5aacab3d0103a7b7e65d6d0d64cd47029d4155c5
SYJ1029 <abg0986@gmail.com>
2025-09-12 11:27:33 +0900

    False Sharing으로 인한 속도 지연이 나타난다

--- MyThread/MyThread/Helloworld.cpp
+++ MyThread/MyThread/Helloworld.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include <stdio.h>
 #include <thread>
 #include <chrono>
 #include <mutex>
+
 
 const int MAX_THREADS = 16;
 
@@ -18,6 +18,32 @@ NUM array_sum[MAX_THREADS] = { 0 };
 std::mutex m;
 
 
+volatile int array_sum2[MAX_THREADS] = { 0 };
+
+
+
+void worker(const int thread_id, const int loop_count)
+{
+	for (int i = 0; i < loop_count; ++i)
+		array_sum2[thread_id] =
+		array_sum2[thread_id] + 2;
+
+}
+
+
+void worker2(int loop)
+{
+	volatile int local_sum = 0;
+	for (auto i = 0; i < loop; ++i)
+		local_sum += 2;
+
+
+	m.lock();
+	sum = sum + local_sum;
+	m.unlock();
+}
+
+
 void worker3(const int thread_id, const int loop_count)
 {
 	for (int i = 0; i < loop_count; ++i)
@@ -25,32 +51,7 @@ void worker3(const int thread_id, const int loop_count)
 		array_sum[thread_id].value + 2;
 }
 
-void worker()
-{
-	for (auto i = 0; i < 5000000; ++i) 
-	{
-		//m.lock();
-		sum += 2;
-		//m.unlock();
-	}
 
-
-}
-
-void worker2(int loop)
-{
-	volatile int local_sum = 0;
-	for (auto i = 0; i < loop; ++i)
-	{
-		//m.lock();
-		local_sum += 2;
-		//m.unlock();
-	}
-
-	m.lock();
-	sum = sum + local_sum;
-	m.unlock();
-}
 
 
 void NoLockWorker()
@@ -66,7 +67,7 @@ int main()
 
 	{
 	
-		for (int num_threads = 1; num_threads <= 16; num_threads *= 2){
+		for (int num_threads = 1; num_threads <= MAX_THREADS; num_threads *= 2){
 			sum = 0;
 			auto timerStart = std::chrono::high_resolution_clock::now();
 			
@@ -96,9 +97,47 @@ int main()
 	
 	}
 
+	std::cout << std::endl;
+
 	{
 
-		for (int num_threads = 1; num_threads <= 16; num_threads *= 2) {
+		for (int num_threads = 1; num_threads <= MAX_THREADS; num_threads *= 2) {
+			sum = 0;
+			auto timerStart = std::chrono::high_resolution_clock::now();
+
+			std::thread* threads = new std::thread[num_threads];
+
+
+			for (int i = 0; i < num_threads; ++i)
+				threads[i] = std::thread(worker, i, 50000000 / num_threads);
+			for (int i = 0; i < num_threads; ++i) {
+				threads[i].join();
+
+				sum += array_sum2[i];
+				array_sum2[i] = 0;
+			}
+
+
+			delete[] threads;
+
+			auto timerEnd = std::chrono::high_resolution_clock::now();
+			auto time_span = duration_cast<milliseconds>(timerEnd - timerStart).count();
+
+			std::cout << "Sum = " << sum << std::endl;
+			std::cout << num_threads << "개의 쓰레드 실행 시간 : " << time_span << "ms" << std::endl;
+
+
+
+			//if (num_threads == 1) --num_threads;
+		}
+	}
+
+	std::cout << std::endl;
+
+
+	{
+
+		for (int num_threads = 1; num_threads <= MAX_THREADS; num_threads *= 2) {
 			sum = 0;
 			auto timerStart = std::chrono::high_resolution_clock::now();
 
