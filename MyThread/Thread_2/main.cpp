4867061e8332a05229ad768921b0bb8977823bd3
DESKTOP-HGMKATH\abg09 <abg0986@gmail.com>
2025-09-22 12:53:18 +0900

    베이커리 알고리즘 진행 중

--- MyThread/Thread_2/main.cpp
+++ MyThread/Thread_2/main.cpp
@@ -1,45 +1,125 @@
-#include <iostream>
 #include <thread>
+#include <mutex>
+#include <atomic>
+#include <algorithm>
+#include <vector>
 
+int threadID = 1;
 
-const int LOOP_COUNT = 5'000'0000;
-volatile int x, y;
-std::atomic<int> trace_x[LOOP_COUNT], trace_y[LOOP_COUNT];
+volatile int sum{ 0 };
 
-void thread_x()
+std::mutex m;
+std::atomic<int> atomSum{ 0 };
+Bakery bake;
+
+void Add_Lock(int loopCount)
 {
-	for(int i = 0; i < LOOP_COUNT; ++i) {
-		x = i;
-		trace_x[i] = y;
+	int localSum{ 0 };
+
+	for (int i = 0; i < loopCount; ++i)
+	{
+		localSum++;
 	}
+
+	m.lock();
+	sum += localSum;
+	m.unlock();
 }
 
-void thread_y()
+void Add_Bakery(int loopCount)
 {
-	for(int i = 0; i < LOOP_COUNT; ++i) {
-		y = i;
-		trace_y[i] = x;
+
+	int id = threadID++;
+
+	int localSum{ 0 };
+
+	for (int i = 0; i < loopCount; ++i)
+	{
+		localSum++;
 	}
+
+	
 }
+
+void Add_atomic(int loopCount)
+{
+	int localSum{ 0 };
+
+	for (int i = 0; i < loopCount; ++i)
+	{
+		localSum++;
+	}
+
+	atomSum += localSum;
+}
+
+
+struct Lock
+{
+	void lock() {}
+	void unlock() {}
+};
+
+
+
+
+class Bakery {
+	bool* flag;
+	std::vector<size_t> label;
+
+public:
+	Bakery() {}
+	void make(int n) {
+		flag = new bool[n];
+		label.reserve(n);
+		for (int i = 0; i < n; i++) {
+			flag[i] = false;
+			label.emplace_back(0);
+		}
+
+	}
+	void lock(int id) {
+		auto i = std::this_thread::get_id();
+
+		flag[id] = true;
+
+		label[id] = *std::max_element(label.begin(), label.end()) + 1;
+		auto k = std::distance(label.begin(), (std::min_element(label.begin(), label.end())));
+		while (true)
+		{
+			if (id != k && flag[k] && label[k] < label[id] && k < id)
+				break;
+		}
+
+		while (k != id && flag[k] && label[k] < label[id] && k < id) {}
+
+	}
+	void unlock(int id) {
+		flag[id] = false;
+
+	}
+
+};
+
+
 
 int main()
 {
-	std::thread t0(thread_x);
-	std::thread t1(thread_y);
+	{
+		// volatile만 사용
+	}
 
-	// Thread 2개 실행
-	int count = 0;
-	for (int i = 0; i < LOOP_COUNT; ++i)
-		if(trace_x[i] == trace_x[i + 1])
-			if (trace_y[trace_x[i]] == trace_y[trace_x[i] + 1]) {
-				if (trace_y[trace_x[i]] != i) continue;
-				else ++count;
-			}
-	std::cout << "Total Memory Inconsistency: " << count << std::endl;
+	{
+		// mutex 사용
+	}
 
+	{
+		// atomic 사용
+	}
 
+	{
+		// Bakery 알고리즘 사용
+		
 
-	t0.join();
-	t1.join();
+	}
 }
-
