e416be46153855662359d574a461717e6163ae07
SYJ1029 <abg0986@gmail.com>
2025-09-23 10:39:33 +0900

    1. 과제는 프로젝트를 분리
    2. Cache Line 문제 실습

--- MyThread/Thread_2/main.cpp
+++ MyThread/Thread_2/main.cpp
@@ -1,125 +1,52 @@
+#include <iostream>
 #include <thread>
-#include <mutex>
-#include <atomic>
-#include <algorithm>
-#include <vector>
 
-int threadID = 1;
 
-volatile int sum{ 0 };
+volatile bool done = false;
+std::atomic<int*> bound;
+int error;
 
-std::mutex m;
-std::atomic<int> atomSum{ 0 };
-Bakery bake;
-
-void Add_Lock(int loopCount)
+void worker()
 {
-	int localSum{ 0 };
-
-	for (int i = 0; i < loopCount; ++i)
+	for (int i = 0; i < 5'000'0000; ++i)
 	{
-		localSum++;
+		*bound = -(1 + *bound);
+
 	}
+	done = true;
 
-	m.lock();
-	sum += localSum;
-	m.unlock();
-}
-
-void Add_Bakery(int loopCount)
-{
-
-	int id = threadID++;
-
-	int localSum{ 0 };
-
-	for (int i = 0; i < loopCount; ++i)
-	{
-		localSum++;
-	}
-
-	
-}
-
-void Add_atomic(int loopCount)
-{
-	int localSum{ 0 };
-
-	for (int i = 0; i < loopCount; ++i)
-	{
-		localSum++;
-	}
-
-	atomSum += localSum;
 }
 
 
-struct Lock
+void monitor()
 {
-	void lock() {}
-	void unlock() {}
-};
-
-
-
-
-class Bakery {
-	bool* flag;
-	std::vector<size_t> label;
-
-public:
-	Bakery() {}
-	void make(int n) {
-		flag = new bool[n];
-		label.reserve(n);
-		for (int i = 0; i < n; i++) {
-			flag[i] = false;
-			label.emplace_back(0);
-		}
-
-	}
-	void lock(int id) {
-		auto i = std::this_thread::get_id();
-
-		flag[id] = true;
-
-		label[id] = *std::max_element(label.begin(), label.end()) + 1;
-		auto k = std::distance(label.begin(), (std::min_element(label.begin(), label.end())));
-		while (true)
+	int error_count = 0;
+	while (!done)
+	{
+		int v = *bound;
+		if ((v != 0) && (v != -1))
 		{
-			if (id != k && flag[k] && label[k] < label[id] && k < id)
-				break;
+			++error_count;
+			printf("%x\n", v);
+			error = v;
 		}
-
-		while (k != id && flag[k] && label[k] < label[id] && k < id) {}
-
-	}
-	void unlock(int id) {
-		flag[id] = false;
-
 	}
 
-};
-
-
+	std::cout << "Error count: " << error_count << ", last error: " << error << std::endl;
+}
 
 int main()
 {
-	{
-		// volatile만 사용
-	}
+	int value[32];
+	long long addr = reinterpret_cast<long long>(&value[16]);
+	addr = (addr / 64) * 64;
+	addr = addr - 2;
+	bound = reinterpret_cast<int*>(addr);
+	std::thread t1(worker);
+	std::thread t2(monitor);
+	t1.join();
+	t2.join();
 
-	{
-		// mutex 사용
-	}
-
-	{
-		// atomic 사용
-	}
-
-	{
-		// Bakery 알고리즘 사용
-		
-
-	}
 }
+
+
---
+++ MyThread/Thread_BakeryHomework/main.cpp
@@ -0,0 +1,125 @@
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <algorithm>
+#include <vector>
+
+int threadID = 1;
+
+volatile int sum{ 0 };
+
+std::mutex m;
+std::atomic<int> atomSum{ 0 };
+Bakery bake;
+
+void Add_Lock(int loopCount)
+{
+	int localSum{ 0 };
+
+	for (int i = 0; i < loopCount; ++i)
+	{
+		localSum++;
+	}
+
+	m.lock();
+	sum += localSum;
+	m.unlock();
+}
+
+void Add_Bakery(int loopCount)
+{
+
+	int id = threadID++;
+
+	int localSum{ 0 };
+
+	for (int i = 0; i < loopCount; ++i)
+	{
+		localSum++;
+	}
+
+
+}
+
+void Add_atomic(int loopCount)
+{
+	int localSum{ 0 };
+
+	for (int i = 0; i < loopCount; ++i)
+	{
+		localSum++;
+	}
+
+	atomSum += localSum;
+}
+
+
+struct Lock
+{
+	void lock() {}
+	void unlock() {}
+};
+
+
+
+
+class Bakery {
+	bool* flag;
+	std::vector<size_t> label;
+
+public:
+	Bakery() {}
+	void make(int n) {
+		flag = new bool[n];
+		label.reserve(n);
+		for (int i = 0; i < n; i++) {
+			flag[i] = false;
+			label.emplace_back(0);
+		}
+
+	}
+	void lock(int id) {
+		auto i = std::this_thread::get_id();
+
+		flag[id] = true;
+
+		label[id] = *std::max_element(label.begin(), label.end()) + 1;
+		auto k = std::distance(label.begin(), (std::min_element(label.begin(), label.end())));
+		while (true)
+		{
+			if (id != k && flag[k] && label[k] < label[id] && k < id)
+				break;
+		}
+
+		while (k != id && flag[k] && label[k] < label[id] && k < id) {}
+
+	}
+	void unlock(int id) {
+		flag[id] = false;
+
+	}
+
+};
+
+
+
+int main()
+{
+	{
+		// volatile만 사용
+	}
+
+	{
+		// mutex 사용
+	}
+
+	{
+		// atomic 사용
+	}
+
+	{
+		// Bakery 알고리즘 사용
+
+
+	}
+}
